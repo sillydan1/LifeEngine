7b0978fee7185e24b24a086c8d707dd2c90288f7
Asger Gitz-Johansen <agitzj16@student.aau.dk>
2021-01-13 19:18:26 +0100

    Timers and tinytimer

--- src/util/Time.h
+++ src/util/Time.h
@@ -1,16 +1,31 @@
-#ifndef LIFE_ENGINE_TIME_H
-#define LIFE_ENGINE_TIME_H
-#include "Timer.hpp"
+/**
+ * Copyright (C) 2021 Asger Gitz-Johansen
 
-namespace life {
+   This file is part of lifeengine.
 
-    struct Time {
-        static double GetGlobalTime();
-        static float GetFrameTime();
-        static void GameStart();
-        static void FrameEnd();
-    };
+    lifeengine is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-}
+    lifeengine is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-#endif //LIFE_ENGINE_TIME_H
+    You should have received a copy of the GNU General Public License
+    along with lifeengine.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+#ifndef LIFEENGINE_TIME_H
+#define LIFEENGINE_TIME_H
+
+struct Time {
+    static double GetGlobalTime();
+    static float GetFrameTime();
+    static void GameStart();
+    static void FrameEnd();
+};
+
+#endif //LIFEENGINE_TIME_H
