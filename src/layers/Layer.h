3110e3e27f6eb2673fbb6752bf679199b10b751b
Asger Gitz-Johansen <agitzj16@student.aau.dk>
2021-01-18 14:55:26 +0100

    Added Layers and LayerCollections classes

---
+++ src/layers/Layer.h
@@ -0,0 +1,42 @@
+/**
+ * Copyright (C) 2021 Asger Gitz-Johansen
+
+   This file is part of lifeengine.
+
+    lifeengine is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    lifeengine is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with lifeengine.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#ifndef LIFEENGINE_LAYER_H
+#define LIFEENGINE_LAYER_H
+#include <lifepch.h>
+#include "events/Event.hpp"
+
+class Layer {
+    friend class LayerStack;
+public:
+    explicit Layer(const std::string& layer_name)
+            : m_dbgname(layer_name) {}
+    virtual ~Layer() {}
+
+    virtual void OnEvent(Event& event) {}
+    virtual void OnUpdate() {}
+    virtual void OnAttach() {}
+    virtual void OnDetach() {}
+    virtual void OnImGuiRender() {}
+
+    inline const std::string& GetName() { return m_dbgname; }
+protected:
+    std::string m_dbgname;
+};
+
+#endif //LIFEENGINE_LAYER_H
