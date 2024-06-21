41dd3f5b57e2615cb982b3ccf78f270ecd96e5a2
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2024-06-21 06:49:34 +0200

    fix: switch to lyra instead of clipp
    clipp seems to be unmaintained and does not work with >C++20

--- src/main.cpp
+++ src/main.cpp
@@ -16,31 +16,38 @@
     You should have received a copy of the GNU General Public License
     along with lifeengine.  If not, see <https://www.gnu.org/licenses/>.
  */
-#include <clipp.h>
+#include <lyra/lyra.hpp>
 #include <spdlog/common.h>
 #include "SANDBOX/Sandbox.h"
 #include "config.h"
 
 int main(const int argc, char* argv[]) {
-    int verbosity = 0;
-    bool version;
-    auto cli = clipp::group(
-        clipp::option("-v", "--vebosity").set(verbosity).doc("set verbosity [0-6]"),
-        clipp::option("-V", "--version").set(version).doc("show version")
-    );
-
-    if(!clipp::parse(argc, argv, cli)) {
-        std::cout << clipp::make_man_page(cli, argv[0]);
+    int verbosity = SPDLOG_LEVEL_WARN;
+    bool show_version = false;
+    bool show_help = false;
+    auto cli = lyra::cli()
+        | lyra::help(show_help).description("A work in progress game engine")
+            ("show this message")
+        | lyra::opt(verbosity, "0-6")
+            ["-v"]["--verbosity"]("set verbosity level, default: " + std::to_string(verbosity))
+        | lyra::opt(show_version)
+            ["-V"]["--version"]("show version")
+        ;
+    auto args = cli.parse({argc, argv});
+    if(!args) {
+        spdlog::error(args.message());
         return 1;
     }
-    if(version) {
+    if(show_help) {
+        std::cout << cli << std::endl;
+        return 0;
+    }
+    if(show_version) {
         std::cout << PROJECT_NAME << " " << PROJECT_VER << std::endl;
         return 0;
     }
-
-    // Start the engine
-    auto engine = Sandbox{};
-    // Start the game
+    spdlog::set_level(static_cast<spdlog::level::level_enum>(std::max(SPDLOG_LEVEL_OFF - verbosity, 0)));
+    spdlog::debug("log level set to {}", verbosity);
     engine.GameStart();
     return 0;
 }
